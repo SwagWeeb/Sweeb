9447dba2063d62566d5939730ddadc6b81d7579d
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-01-28 22:12:33 -0500

    Update global.js

--- src/functions/global.js
+++ src/functions/global.js
@@ -10,6 +10,14 @@ module.exports = (client) => {
     client.global.categories = ["Pat", "Hug", "Kiss", "Highfive", "Heart", "Cry"],
     client.global.isUrl = require('./isUrl'),
 
+    client.clean = async (client, text) => {
+        if (text && text.constructor.name == "Promise") text = await text;
+        if (typeof evaled !== "string") text = require("util").inspect(text, {
+            depth: 1
+         });
+          return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203) + String.fromCharCode(8203)).replace(client.token, "I have a structured settlement and I need cash now.\nCall J. G. Wentworth!\n877 cash now\nI have an annuity but I need cash now.\nCall J. G. Wentworth!\n877 cash now\n877 cash now\nThey've helped thousands, they'll help you, too.\nOne lump sum of cash they will pay to you.\nIf you get long term payments but you need cash now\nCall J. G. Wentworth\n877 cash now\nCall Now");
+      },
+
     client.permlevel = (message) => {
         let permlvl = 0;
         const permOrder = client.config.permLevels.slice(0).sort((p, c) => p.level < c.level ? 1 : -1);
@@ -22,7 +30,7 @@ module.exports = (client) => {
             }
         }
         return permlvl;
-    };
+    },
 
     String.prototype.toProperCase = function() {
         return this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
