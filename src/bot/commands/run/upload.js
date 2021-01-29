d4656aa871db54aa3446eb1cae806e7a080ce001
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-01-28 22:00:46 -0500

    Update upload.js

--- src/bot/commands/run/upload.js
+++ src/bot/commands/run/upload.js
@@ -5,15 +5,15 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     if (!client.global.categories.includes(args[0].toProperCase())) return client.global.message.error(message, "not_category", `Not a valid category!\n do \`sw!categories\` to see a list!`, "(NOT_A_CATEGORY)");
     if (!client.global.isUrl(args[1])) return client.global.message.error(message, "not_url", "Not a valid url!", "(NOT_A_URL)");
     
-    if ((typeof args[2].toLowerCase()) === "boolean") {
-        const date = new Date();
-        client.global.db.query(`INSERT INTO 
+    var val = (args[2].toLowerCase() === false);
+    if (typeof val !== "boolean") return client.global.message.error(message, "not_bool", "Not a boolean! for NSFW tag use \`true/false\`!", "(NOT_A_BOOLEAN)");
+    const date = new Date();
+    client.global.db.query(`INSERT INTO 
     sweebData 
     (id, category, nsfw, date, fileLink) 
     VALUES (${client.global.escapeDB(client.global.createID(30))}, ${client.global.escapeDB(args[0].toProperCase())}, ${client.global.escapeDB(args[2].toLowerCase())}, ${date}, ${client.global.escapeDB(args[1])})`)
 
     client.global.message.success(message, "Upload", `Successfully uploaded the image to /\`${args[0]}\`/`);
-    } else return client.global.message.error(message, "not_bool", "Not a boolean! for NSFW tag use \`true/false\`!", "(NOT_A_BOOLEAN)");
   };
   
   exports.conf = {
