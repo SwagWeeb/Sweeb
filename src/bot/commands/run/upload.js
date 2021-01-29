0747cd6650a0ff5554647f98c34763786e92e28b
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-01-29 00:02:02 -0500

    Update upload.js

--- src/bot/commands/run/upload.js
+++ src/bot/commands/run/upload.js
@@ -13,6 +13,9 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     
     client.global.db.query(`INSERT INTO 
     sweebData (id, category, nsfw, date, fileLink) VALUES (${client.global.createId(30)}, ${args[0].toProperCase()}, ${args[2].toLowerCase()}, ${datetime}, ${client.global.escapeDB(args[1])})`)
+    .on('result', function (row) {
+      console.log(row);
+    })
     .on('error', function (err) {
       console.log(err);
     });
