dbbd0585e58e21977b1f2219d617e7e44c75b8eb
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-01-30 10:01:05 -0500

    pog

--- src/bot/commands/run/upload.js
+++ src/bot/commands/run/upload.js
@@ -9,9 +9,9 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     //var val = args[2].toLowerCase() === false;
     //if (typeof val !== "boolean") return client.global.message.error(message, "not_bool", "Not a boolean! for NSFW tag use \`true/false\`!", "(NOT_A_BOOLEAN)");
     var dayjs = require('dayjs')
-    const timestamp = dayjs(new Date()).format("YYYY-MM-DD");
+    const timestamp = dayjs(new Date()).format("YYYY,MM,DD");
     
-    client.global.db.query(`INSERT INTO sweebData (id, category, nsfw, dateAdded, fileLink) VALUES (${client.global.createId(14)}, "${args[0].toProperCase()}", false, STR_TO_DATE(${timestamp}, '%Y-%m-%d'), "${args[1]}")`)
+    client.global.db.query(`INSERT INTO sweebData (id, category, nsfw, dateAdded, fileLink) VALUES (${client.global.createId(14)}, "${args[0].toProperCase()}", false, STR_TO_DATE(${timestamp}, '%Y %m %d'), "${args[1]}")`)
 
 
     client.global.message.success(message, "Upload", `Successfully uploaded the image to /\`${args[0]}\`/`);
