af76c17539d5b2ef20e607f97b4301a3be1361b2
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-01-30 10:34:36 -0500

    yea

--- src/routes/Api.js
+++ src/routes/Api.js
@@ -12,7 +12,7 @@ router.get('/:category', async function(req, res) {
     log.log(`[Sweeb] /${categoryFix.toUpperCase()}/ requested by ${ip}`)
     if (!req.headers.authorization) return res.status(403).json({ error: 'no_token' });
     if (req.headers.authorization !== process.env.SERVER_KEY) return res.status(403).json({ error: 'unauthorized' });
-    db.query(`SELECT * FROM sweebData WHERE category = "Pat"`, function(err, data) {
+    db.query(`SELECT * FROM sweebData WHERE category = "${categoryFix.toProperCase()}"`, function(err, data) {
         const pic = data[Math.floor(Math.random()*data.length)]
         return res.json({url: pic.fileLink, id: pic.id, category: pic.category, added: pic.dateAdded});
     })
