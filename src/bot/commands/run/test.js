5fb51c96969c7e437b5f8a358fa548972ae0084d
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-01-30 09:36:42 -0500

    Update test.js

--- src/bot/commands/run/test.js
+++ src/bot/commands/run/test.js
@@ -2,10 +2,8 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 
   const test = "";
 client.global.db.query(`SELECT * FROM sweebData`, function (error, memberData) {
-  for (let i = 0; i < memberData.length; i++) {
-    client.global.log.log(memberData[i].url)
-  }
-  client.global.log.log(memberData[0].id)
+  console.log(error);
+  console.log(memberData);
 })
   };
   
