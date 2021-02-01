67ef07e7f8a464f777b13b545990389dc510e648
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-02-01 18:05:56 -0500

    Update upload.js

--- src/bot/commands/run/upload.js
+++ src/bot/commands/run/upload.js
@@ -12,6 +12,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
           const timestamp = dayjs(new Date()).format("YYYY,MM,DD");
           client.global.db.query(`INSERT INTO sweebData (id, category, nsfw, dateAdded, fileLink) VALUES (${client.global.createId(14)}, "${args[0].toProperCase()}", false, '${timestamp}', "${args[1]}")`);
           client.global.message.success(message, "channel", "Upload", `Successfully uploaded the image to /\`${args[0]}\`/`);
+          client.global.log.log(`[Sweeb] /${args[0]}/${args[1]}/ uploaded by ${message.author.tag}`)
         }
     })
 };
