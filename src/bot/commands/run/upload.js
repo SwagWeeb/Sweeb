976fd7819cfcd8b842057cc567a38b4e02d4badb
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-02-01 17:45:52 -0500

    Update upload.js

--- src/bot/commands/run/upload.js
+++ src/bot/commands/run/upload.js
@@ -4,20 +4,21 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     if (!args[1]) return client.global.message.error(message, `You did not provide any arguments! \`sw!upload <Link> <Category>\``, "(NO_ARG)");
     if (!client.global.categories.includes(args[0].toProperCase())) return client.global.message.error(message, `Not a valid category!\n do \`sw!categories\` to see a list!`, "(NOT_A_CATEGORY)");
     if (!client.global.isUrl(args[1])) return client.global.message.error(message, "Not a valid url!", "(NOT_A_URL)");
-    const test = false;
+    var isAdded = false;
     client.global.db.query(`SELECT * FROM sweebData where category = '${args[0].toProperCase()}'`, function(err, data) {
-      if (!data) return; // silently return nothing as database may not be initialized
-      for (var i = 0; i < data.length; i++) {
-        const pic = data[i];
-        if (pic.fileLink == args[1]) return client.global.message.error(message, `Link already present in Database under \`${pic.category}\``, "(LINK_IN_DB)") 
-        else continue;
-      }
-  })
-    var dayjs = require('dayjs')
-    const timestamp = dayjs(new Date()).format("YYYY,MM,DD");
-    client.global.db.query(`INSERT INTO sweebData (id, category, nsfw, dateAdded, fileLink) VALUES (${client.global.createId(14)}, "${args[0].toProperCase()}", false, '${timestamp}', "${args[1]}")`);
-    client.global.message.success(message, "channel", "Upload", `Successfully uploaded the image to /\`${args[0]}\`/`);
-  };
+        if (!data) return; // silently return nothing as database may not be initialized
+        for (var i = 0; i < data.length; i++) {
+            const pic = data[i];
+            if (pic.fileLink == args[1]) return isAdded = true;
+        }
+        if (!isAdded) {
+            var dayjs = require('dayjs')
+            const timestamp = dayjs(new Date()).format("YYYY,MM,DD");
+            client.global.db.query(`INSERT INTO sweebData (id, category, nsfw, dateAdded, fileLink) VALUES (${client.global.createId(14)}, "${args[0].toProperCase()}", false, '${timestamp}', "${args[1]}")`);
+            client.global.message.success(message, "channel", "Upload", `Successfully uploaded the image to /\`${args[0]}\`/`);
+        } else return client.global.message.error(message, `Link already present in Database under \`${pic.category}\``, "(LINK_IN_DB)")
+    })
+};
   
   exports.conf = {
     enabled: true,
