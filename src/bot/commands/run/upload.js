3f8932725b8b5b35af28a40f32e79d60469c9dc9
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-02-01 17:48:17 -0500

    Update upload.js

--- src/bot/commands/run/upload.js
+++ src/bot/commands/run/upload.js
@@ -11,13 +11,13 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
             const pic = data[i];
             if (pic.fileLink == args[1]) return isAdded = true;
         }
-        if (!isAdded) {
-            var dayjs = require('dayjs')
-            const timestamp = dayjs(new Date()).format("YYYY,MM,DD");
-            client.global.db.query(`INSERT INTO sweebData (id, category, nsfw, dateAdded, fileLink) VALUES (${client.global.createId(14)}, "${args[0].toProperCase()}", false, '${timestamp}', "${args[1]}")`);
-            client.global.message.success(message, "channel", "Upload", `Successfully uploaded the image to /\`${args[0]}\`/`);
-        } else return client.global.message.error(message, `Link already present in Database under \`${pic.category}\``, "(LINK_IN_DB)")
     })
+    if (!isAdded) {
+      var dayjs = require('dayjs')
+      const timestamp = dayjs(new Date()).format("YYYY,MM,DD");
+      client.global.db.query(`INSERT INTO sweebData (id, category, nsfw, dateAdded, fileLink) VALUES (${client.global.createId(14)}, "${args[0].toProperCase()}", false, '${timestamp}', "${args[1]}")`);
+      client.global.message.success(message, "channel", "Upload", `Successfully uploaded the image to /\`${args[0]}\`/`);
+  } else return client.global.message.error(message, `Link already present in Database under \`${pic.category}\``, "(LINK_IN_DB)")
 };
   
   exports.conf = {
