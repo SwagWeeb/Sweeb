d410b4623dc2c8ae4afb4ea273d021fabebe0d00
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-02-01 17:58:50 -0500

    Update upload.js

--- src/bot/commands/run/upload.js
+++ src/bot/commands/run/upload.js
@@ -4,18 +4,16 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     if (!args[1]) return client.global.message.error(message, `You did not provide any arguments! \`sw!upload <Link> <Category>\``, "(NO_ARG)");
     if (!client.global.categories.includes(args[0].toProperCase())) return client.global.message.error(message, `Not a valid category!\n do \`sw!categories\` to see a list!`, "(NOT_A_CATEGORY)");
     if (!client.global.isUrl(args[1])) return client.global.message.error(message, "Not a valid url!", "(NOT_A_URL)");
-    var isAdded = false;
     client.global.db.query(`SELECT * FROM sweebData where fileLink = ${client.global.escapeDB(args[1])}`, function(err, data) {
         if (!data) return; // silently return nothing as database may not be initialized
-        if (data[0].fileLink == args[1]) isAdded = true;
+        if (data[0].fileLink == args[1]) return client.global.message.error(message, `Link already present in Database under \`${pic.category}\``, "(LINK_IN_DB)")
+        else {
+          var dayjs = require('dayjs')
+          const timestamp = dayjs(new Date()).format("YYYY,MM,DD");
+          client.global.db.query(`INSERT INTO sweebData (id, category, nsfw, dateAdded, fileLink) VALUES (${client.global.createId(14)}, "${args[0].toProperCase()}", false, '${timestamp}', "${args[1]}")`);
+          client.global.message.success(message, "channel", "Upload", `Successfully uploaded the image to /\`${args[0]}\`/`);
+        }
     })
-  //   if (!isAdded) {
-  //     var dayjs = require('dayjs')
-  //     const timestamp = dayjs(new Date()).format("YYYY,MM,DD");
-  //     client.global.db.query(`INSERT INTO sweebData (id, category, nsfw, dateAdded, fileLink) VALUES (${client.global.createId(14)}, "${args[0].toProperCase()}", false, '${timestamp}', "${args[1]}")`);
-  //     client.global.message.success(message, "channel", "Upload", `Successfully uploaded the image to /\`${args[0]}\`/`);
-  // } else return client.global.message.error(message, `Link already present in Database under \`${pic.category}\``, "(LINK_IN_DB)")
-  console.log(isAdded);
 };
   
   exports.conf = {
