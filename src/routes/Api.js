dd6414e2a27519cdacf330353d6ae4562fb65cc4
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-02-05 12:41:31 -0500

    Update route to have a more secure MYSQL usage

--- src/routes/Api.js
+++ src/routes/Api.js
@@ -6,34 +6,31 @@ const db = require('../database/mysql');
 router.get('/:category', async function(req, res) {
     if (!req.params) return res.status(403).json({ error: 'Choose a correct path' });
     const categoryFix = req.params.category;
-
     if (!res.locals.bot.global.categories.includes(categoryFix.toProperCase())) return res.status(401).json({ error: 'unauthorized' });
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    const who = req.headers['user-agent'] || "Undefined (1.0.0)";
-    //console.log(req.headers);
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress, who = req.headers['user-agent'] || "Undefined (1.0.0)";
     log.log(`[Sweeb] /${categoryFix.toUpperCase()}/ requested by ${ip} - ${who}`)
     if (!req.headers.authorization) return res.status(401).json({ error: 'no_token' });
     db.query(`SELECT * FROM sweebAPI WHERE apiToken = "${req.headers.authorization}"`, function(err, data) {
         if (data == undefined) return res.status(401).json({ error: 'unauthorized' });
-        else db.query(`SELECT * FROM sweebData WHERE category = "${categoryFix.toProperCase()}"`, function(err, data) {
+        else db.query(`(SELECT * FROM sweebData WHERE category = "${categoryFix.toProperCase()}" ORDER BY RAND() LIMIT 1)`, function(err, data) {
             if (data == undefined) return res.staus(400).json({ error: 'data_not_found'})
-            const pic = data[Math.floor(Math.random()*data.length)]
-            if (pic == undefined) return res.status(400).json({ error: 'data_not_found'})
-            return res.json({url: pic.fileLink, id: pic.id, category: pic.category, added: pic.dateAdded});
+            const picData = data[0];
+            if (picData == undefined) return res.status(400).json({ error: 'data_not_found'})
+            return res.json({url: picData.fileLink, id: picData.id, category: picData.category, added: picData.dateAdded});
         })
     })
 })
 
-router.get('/upload/:ID/:File', function(req, res) {
-    if (!req.headers.authorization) return res.status(403).json({ error: 'no_token' });
-    if (req.headers.authorization !== process.env.SERVER_KEY) return res.status(403).json({ error: 'unauthorized' });
-    if (!req.locals.bot.config.uploaders.includes(req.params.IDS)) return res.status(403).json({ error: 'unauthorized' });
-})
-
-router.get('/delete/:ID/:File', function(req, res) {
-    if (!req.headers.authorization) return res.status(403).json({ error: 'no_token' });
-    if (req.headers.authorization !== process.env.SERVER_KEY) return res.status(403).json({ error: 'unauthorized' });
-    if (!req.locals.bot.config.staffIDS.includes(req.params.IDS)) return res.status(403).json({ error: 'unauthorized' });
+router.get('/categories/:Type', function(req, res) {
+    const categoryFix = req.params.Type;
+    if (req.params) {
+        if (!res.locals.bot.global.categories.includes(categoryFix.toProperCase())) return res.status(401).json({ error: 'unauthorized' });
+        db.query(`SELECT * FROM sweebData where category = "${categoryFix.toProperCase()}"`, function(err, data) {
+            if (data || data[0] || data.length == undefined) return res.staus(400).json({ error: 'data_not_found'})
+            return res.json({category: categoryFix.toProperCase(), amount: data.length});
+        });
+    } else return res.json({categories: res.locals.bot.global.categories});
+    
 })
 
 module.exports = router;
