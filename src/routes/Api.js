6572626cd8420ea2f468abb4134f9fc3b1e26eaf
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-02-05 18:01:23 -0500

    Update Api.js

--- src/routes/Api.js
+++ src/routes/Api.js
@@ -23,13 +23,15 @@ router.get('/:category', async function(req, res) {
 
 router.get('/categories/:Type', function(req, res) {
     const categoryFix = req.params.Type;
-    if (categoryFix || categoryFix !== undefined) {
+    if (!categoryFix || categoryFix == undefined) {
+        return res.json({categories: res.locals.bot.global.categories});
+    } else {
         if (!res.locals.bot.global.categories.includes(categoryFix.toProperCase())) return res.status(401).json({ error: 'unauthorized' });
         db.query(`SELECT * FROM sweebData where category = "${categoryFix.toProperCase()}"`, function(err, data) {
             if (!data || !data[0] || data.length == undefined) return res.status(400).json({ error: 'data_not_found'})
             return res.json({category: categoryFix.toProperCase(), amount: data.length});
         });
-    } else return res.json({categories: res.locals.bot.global.categories});
+    }
     
 })
 
